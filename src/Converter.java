6f3d1b7c966755015e3b138f46b44a7fceedcd8f
Storrm01 <81132531+Storrm01@users.noreply.github.com>
2025-01-31 05:36:09 +0000

    Final Commit

--- src/Converter.java
+++ src/Converter.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+public class Converter{
+    public static void main(String[] ags){
+        // Convert the string strFloat into a float.
+String strFloat = "374.25";
+float from_strFloat = (float) str_float; // assign here
+
+// Convert the string strInt into an integer.
+String strInt = "1250";
+int from_strInt = (int) strInt; // assign here
+
+// Convert the float floatNum into an integer.
+float floatNum = 56.715f;
+int from_floatNum = (int) floatNum; // assign here
+
+// Convert the integer intNum into a float.
+int intNum = 1500;
+float from_intNum = (float) from_intNum; // assign here
+
+// Convert the double doubleNum into a string.
+double doubleNum = 3.14159265;
+String from_doubleNum = (String) doubleNum; // assign here
+    }
+}
